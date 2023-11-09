29763bdaa3ecb6aecec4ea4aab07e103c59f0895
Yuxuan Zuo <146106195+3053887951@users.noreply.github.com>
2023-11-09 23:21:12 +0000

    Update employee.py

--- employee.py
+++ employee.py
@@ -16,11 +16,11 @@ class Employee:
         if self.pay_type == 'monthly':
             return self.base_pay
         elif self.pay_type == 'hourly':
-            return self.hours_worked * self.hourly_rate
+            return self.hours_worked * self.hourly_rate if self.hours_worked is not None and self.hourly_rate is not None else 0
         elif self.pay_type == 'contract':
-            contract_pay = self.hours_worked * self.hourly_rate if self.hours_worked else 0
-            commission_pay = self.contracts * self.commission_rate if self.contracts else 0
-            bonus_pay = self.bonus if self.bonus else 0
+            contract_pay = self.hours_worked * self.hourly_rate if self.hours_worked is not None and self.hourly_rate is not None else 0
+            commission_pay = self.contracts * self.commission_rate if self.contracts is not None and self.commission_rate is not None else 0
+            bonus_pay = self.bonus if self.bonus is not None else 0
             return contract_pay + commission_pay + bonus_pay
 
     def __str__(self):
@@ -29,7 +29,11 @@ class Employee:
         elif self.pay_type == 'hourly':
             return f'{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour.\nTheir total pay is {self.get_pay()}.'
         elif self.pay_type == 'contract':
-            return f'{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour and receives a commission for {self.contracts} contract(s) at {self.commission_rate}/contract.\nTheir total pay is {self.get_pay() + self.bonus}.'
+            contract_pay = self.hours_worked * self.hourly_rate if self.hours_worked is not None and self.hourly_rate is not None else 0
+            commission_pay = self.contracts * self.commission_rate if self.contracts is not None and self.commission_rate is not None else 0
+            bonus_pay = self.bonus if self.bonus is not None else 0
+            total_pay = contract_pay + commission_pay + bonus_pay
+            return f'{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour and receives a commission for {self.contracts} contract(s) at {self.commission_rate}/contract.\nTheir total pay is {total_pay}.'
 
 # Test cases
 # (Assuming the correct test cases are provided, the following instances should pass the tests)
